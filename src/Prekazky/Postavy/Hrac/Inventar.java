9f1d1fc637a8dd506d79cb103d8fda621b7bea62
FilipDavid1 <filipenkodavid@gmail.com>
2024-04-23 17:49:08 +0200

    change inventory to only store Vec

---
+++ src/Prekazky/Postavy/Hrac/Inventar.java
@@ -0,0 +1,48 @@
+package Prekazky.Postavy.Hrac;
+
+import Veci.Ingrediencie.Ingrediencia;
+import Veci.Vec;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Inventar<E extends Vec> {
+    private ArrayList< E> veci;
+
+    public Inventar() {
+        this.veci = new ArrayList<>();
+    }
+
+    public void pridajVec(E vec) {
+        this.veci.add(vec);
+    }
+
+    public void odstranVec(Vec vec) {
+        this.veci.remove(vec);
+    }
+
+    public void odstranVeci(ArrayList<Ingrediencia> veci) {
+        for (Vec vec : veci) {
+            this.odstranVec(vec);
+        }
+    }
+
+    public Vec getVec(String vec) {
+        for (E e : veci) {
+            if (e.getNazov().equals(vec)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public boolean obsahujeVec(Vec vec) {
+        return this.veci.contains(vec);
+    }
+
+    public void getVeci() {
+        for (Vec vec : veci) {
+            System.out.println(vec.getNazov());
+        }
+    }
+}
