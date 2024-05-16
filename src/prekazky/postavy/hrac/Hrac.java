0d677d4a722c9989bdb9409d09e26ea3554d23a9
FilipDavid1 <filipenkodavid@gmail.com>
2024-05-16 18:59:34 +0200

    Feature: Add keyboard-triggered potion usage methods to Hrac class

--- src/prekazky/postavy/hrac/Hrac.java
+++ src/prekazky/postavy/hrac/Hrac.java
@@ -5,6 +5,12 @@ import mapa.Mapa;
 import prekazky.postavy.OrientaciaPostavy;
 import prekazky.postavy.Postava;
 import prekazky.postavy.monstra.IMonstrum;
+import prekazky.postavy.monstra.Skeleton;
+import veci.elixiry.ElixirMoci;
+import veci.elixiry.ElixirOzivenia;
+import veci.elixiry.ElixirOdolnosti;
+import veci.elixiry.ElixirObnovenia;
+import veci.elixiry.ElixirRegeneracie;
 import veci.ingrediencie.Ingrediencia;
 import veci.IVec;
 import fri.shapesge.Kruh;
@@ -20,14 +26,26 @@ public class Hrac extends Postava {
 
     private Postava vybrataPostava;
 
+    private int casRegeneracie;
+
     public Hrac(int pocetObrazkovIdle, int pocetObrazkovWalk, String nazov, int x, int y, Mapa mapa, Manazer manazer) {
-        super(pocetObrazkovIdle, nazov, x, y);
+        super(pocetObrazkovIdle, nazov, x, y, 10);
         this.mapa = mapa;
         this.kruh = new Kruh();
         this.inventar = new Inventar();
         this.interakcia = new Interakcia(mapa, this.inventar, this);
         manazer.spravujObjekt(this.interakcia);
         this.vybrataPostava = null;
+
+
+        //pridaj elixir moci
+        this.inventar.pridajVec(new ElixirObnovenia("ElixirObnovenia"));
+        this.inventar.pridajVec(new ElixirMoci("ElixirMoci"));
+        this.inventar.pridajVec(new ElixirOdolnosti("ElixirOdolnosti"));
+        this.inventar.pridajVec(new ElixirOzivenia("ElixirOzivenia"));
+        this.inventar.pridajVec(new ElixirRegeneracie("ElixirRegeneracie"));
+
+
     }
 
     public void chodDole() {
@@ -109,6 +127,13 @@ public class Hrac extends Postava {
         if (super.getCasOslabenia() > 0) {
             super.oslabenie((super.getCasOslabenia() / 1000) - 1);
         }
+
+        if (casRegeneracie > 0) {
+            if (casRegeneracie % 1000 == 0) {
+                super.pridajHp(5);
+            }
+            casRegeneracie -= 200;
+        }
     }
 
     public boolean vyrobElixir(Ingrediencia[] potrebneIngrediencie) {
@@ -132,7 +157,7 @@ public class Hrac extends Postava {
 
 
     public void utocNaMonstra() {
-        inventar.getVeci();
+        System.out.println(super.getSila());
         if (this.vybrataPostava != null) {
             double vzdialenost = Math.sqrt(Math.pow(vybrataPostava.getX() - this.getX(), 2) + Math.pow(vybrataPostava.getY() - this.getY(), 2));
             if (vzdialenost < 200) {
@@ -149,7 +174,15 @@ public class Hrac extends Postava {
     @Override
     public void interakcia(Postava postava) {
         if (postava instanceof IMonstrum ) {
-            postava.uberHp(10);
+            if (postava instanceof Skeleton skeleton) {
+                if (!skeleton.jeBarieraAktivna()) {
+                    postava.uberHp(super.getSila());
+                } else {
+                    skeleton.znicBarieru();
+                }
+            } else {
+                postava.uberHp(super.getSila());
+            }
             this.attackAnimacia(super.getCestaKObrazku().replace("/Idle/Idle_Down_0", "/Attack/Attack_") + super.getOrientacia() + "_", 6 );
         } else {
             postava.interakcia(this);
@@ -165,4 +198,52 @@ public class Hrac extends Postava {
         this.inventar.zobrazIngrediencieVInventari();
     }
 
+    public void pouziElixir1() {
+        inventar.pouziElixir(0, this);
+    }
+
+    public void pouziElixir2() {
+        inventar.pouziElixir(1, this);
+    }
+
+    public void pouziElixir3() {
+        inventar.pouziElixir(2, this);
+    }
+
+    public void pouziElixir4() {
+        inventar.pouziElixir(3, this);
+    }
+
+    public void pouziElixir5() {
+        inventar.pouziElixir(4, this);
+    }
+
+    public void pouziElixir6() {
+        inventar.pouziElixir(5, this);
+    }
+
+    public void pouziElixir7() {
+        inventar.pouziElixir(6, this);
+    }
+
+    public void pouziElixir8() {
+        inventar.pouziElixir(7, this);
+    }
+
+    public void pouziElixir9() {
+        inventar.pouziElixir(8, this);
+    }
+
+    public void pouziElixir10() {
+        inventar.pouziElixir(9, this);
+    }
+
+
+    public Postava getVybrataPostava() {
+        return this.vybrataPostava;
+    }
+
+    public void regenerujHp(int sekundy) {
+        this.casRegeneracie = sekundy * 1000;
+    }
 }
