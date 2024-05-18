b47d03d50d50d89976c08e3432ec82e49b949663
FilipDavid1 <filipenkodavid@gmail.com>
2024-05-18 19:47:49 +0200

    Update: fix alchemist potion creation system

--- src/prekazky/postavy/npc/Alchymista.java
+++ src/prekazky/postavy/npc/Alchymista.java
@@ -10,11 +10,11 @@ import javax.swing.JOptionPane;
 import java.lang.reflect.Constructor;
 
 public class Alchymista extends Postava {
-    private final String[] elixiry = {"ElixirEnergie", "ElixirMoci", "ElixirMudrosti", "ElixirObnovenia", "ElixirObrany", "ElixirOdolnosti", "ElixirOdstraneniaKliadby", "ElixirOzivenia", "ElixirRegeneracie", "ElixirRychlosti", "ElixirSily", "ElixirSkrytia"};
+    private final String[] elixiry = {"ElixirJedu", "ElixirMoci", "ElixirMudrosti", "ElixirObnovenia", "ElixirObrany", "ElixirOdolnosti", "ElixirOdstraneniaKliadby", "ElixirOzivenia", "ElixirRegeneracie", "ElixirSily", "ElixirSkrytia"};
     private final BlokTextu blokTextu;
     public Alchymista(int pocetObrazkov, String cestaKObrazku, int x, int y, Postava hrac) {
         super(pocetObrazkov, cestaKObrazku + "0", x, y, 0);
-        this.blokTextu = new BlokTextu("", 0, 30);
+        this.blokTextu = new BlokTextu("", 450, 30);
         this.blokTextu.zmenFont("Courier New", StylFontu.BOLD, 15);
     }
 
@@ -23,13 +23,19 @@ public class Alchymista extends Postava {
         Hrac hrac = (Hrac)postava;
         StringBuilder elixiryNaNovychRiadkoch = new StringBuilder();
         for (String elixir : this.elixiry) {
-            elixiryNaNovychRiadkoch.append(elixir).append("\n");
+            //vypis meno elixiru aj jeho popis
+            try {
+                Class<?> clazz = Class.forName("veci.elixiry." + elixir);
+                Constructor<?> ctor = clazz.getConstructor(String.class);
+                Elixir elixirObj = (Elixir)ctor.newInstance(elixir);
+                elixiryNaNovychRiadkoch.append(elixirObj.getNazov()).append(" - ").append(elixirObj.popisElixira()).append("\n");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        this.blokTextu.zmenText("Ponukane elixiry: \n" + elixiryNaNovychRiadkoch);
-        this.blokTextu.zmenFarbu("white");
-        this.blokTextu.zobraz();
 
-        String nazovElixiru = JOptionPane.showInputDialog(null, "Zadaj nazov elixiru ktory chces Vyrobit: ");
+
+        String nazovElixiru = JOptionPane.showInputDialog(null, "Zadaj nazov elixiru ktory chces Vyrobit: \n" + elixiryNaNovychRiadkoch);
         if (nazovElixiru != null) {
             Elixir elixir = vyrobitElixir(nazovElixiru, hrac.getInventar());
             if (elixir != null) {
@@ -61,5 +67,9 @@ public class Alchymista extends Postava {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void tik() {
+        super.idleAnimacia(super.getCestaKObrazku().replace("0", ""));
     }
 }
